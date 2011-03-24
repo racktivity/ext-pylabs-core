83eed42da37289443c6b38464f39fcfce4f6f653
grypdonp <devnull@localhost>
2011-03-24 17:11:40 +0100

    And even more sampleapp implementation

--- pyapps/sampleapp/impl/schedule/config/pop3/pop3_popMailbox.py
+++ pyapps/sampleapp/impl/schedule/config/pop3/pop3_popMailbox.py
@@ -3,6 +3,7 @@ __author__ = 'incubaid'
 
 def main(q, i, p, params, tags):
     import poplib
+    import base64
     pop3guid = p.api.model.config.pop3.find(server="pop.gmail.com", login="analytics@incubaid.com")['result'][0]
     pop3 = p.api.model.config.pop3.getObject(pop3guid)
     mailbox = poplib.POP3_SSL(pop3.server)
@@ -10,8 +11,9 @@ def main(q, i, p, params, tags):
     mailbox.pass_(pop3.password)
     numMessages = len(mailbox.list()[1])
     for i in range(numMessages):
-        for j in mailbox.retr(i+1)[1]:
-            print j
+        for mail in mailbox.retr(i+1)[1]:
+            print mail
+            p.events.publish('pylabs.event.sampleapp.email', 'mail:%s'%base64.encodestring(mail))
 
 def match(q, i, params, tags):
     import time
