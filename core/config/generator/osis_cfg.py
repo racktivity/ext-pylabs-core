7f3d2be2346d968503c67eb572e4b3808e94e42e
Jens Geiregat <jens.geiregat@incubaid.com>
2011-03-25 18:06:16 +0100

    Osis connection: Use postgres user for now and only create the config file if it does not exist yet

--- core/config/generator/osis_cfg.py
+++ core/config/generator/osis_cfg.py
@@ -1,15 +1,20 @@
+import os
+
 from pylabs import q
 
 class OsisPyApps:
 
     def __init__(self, appName):
         self.appName = appName
-        self.components = [('passwd', 'pass123'), ('login', 'qbase'), 
+        self.components = [('passwd', 'pass123'), ('login', 'postgres'),
                         ('database', self.appName),('ip', '127.0.0.1')]
 
     def generate_cfg(self):
         iniFile = q.system.fs.joinPaths(q.dirs.cfgDir, 'osisdb.cfg')
-        ini = q.tools.inifile.new(iniFile)
+        if os.path.isfile(iniFile):
+            ini = q.tools.inifile.get(iniFile)
+        else:
+            ini = q.tools.inifile.new(iniFile)
 
         exists = ini.checkSection(self.appName)
         if not exists:
