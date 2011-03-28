22c67c7141792366251e76c9f400a70a3b8b837a
bonnek <devnull@localhost>
2011-03-28 18:12:16 +0200

    Updated SQL queries to domain enabled osis views

--- pyapps/sampleapp/impl/action/core/job/clear/job_clear.py
+++ pyapps/sampleapp/impl/action/core/job/clear/job_clear.py
@@ -4,12 +4,8 @@ __priority__= 3
 
 def main(q, i, p, params, tags):
     
-    p.api.model.core.job.query("DELETE FROM job.main;SELECT True")
-    p.api.model.core.job.query("DELETE FROM job.view_job_list;SELECT True")
-    p.api.model.core.job.query("DELETE FROM job.view_job_parentlist;SELECT True")
-    #clear policyrun entries as well
-    p.api.model.core.job.query("DELETE FROM policyrun.main;SELECT True")
-    p.api.model.core.job.query("DELETE FROM policyrun.view_policyrun_list;SELECT True")
+    p.api.model.core.job.query("DELETE FROM core_job.core_view_job_list;SELECT True")
+    p.api.model.core.job.query("DELETE FROM core_job.core_view_job_parentlist;SELECT True")
     
     params['result'] = True
     
