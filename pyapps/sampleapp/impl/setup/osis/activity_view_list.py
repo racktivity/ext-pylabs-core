a786ff999119b1c5c09830a90346eefa5715d13a
dewolft <devnull@localhost>
2011-03-29 17:22:30 +0200

    update osis connection

--- pyapps/sampleapp/impl/setup/osis/activity_view_list.py
+++ pyapps/sampleapp/impl/setup/osis/activity_view_list.py
@@ -8,7 +8,7 @@ def main(q, i, params, tags):
     rootobject = 'activity'
     domain = "crm"
     view_name = '%s_view_%s_list' % (domain, rootobject)
-    connection = OsisDB().getConnection('main')
+    connection = OsisDB().getConnection(p.api.appname)
     if not connection.viewExists(domain, rootobject, view_name):
         view = connection.viewCreate(rootobject, view_name)
         view.setCol('name', q.enumerators.OsisType.STRING, False)
