5c909f3fdaca7319cb68be2d3ca43d51b095ba7b
Jo De Boeck <jdeboeck@aserver.com>
2011-03-29 19:40:28 +0200

    Added missing file for authentication serice

---
+++ lib/actionservice.py
@@ -0,0 +1,34 @@
+from pylabs import q, i, p
+import os
+basedir = os.path.join(q.dirs.pyAppsDir, p.api.appname)
+
+class ActionService:
+    
+    _authenticate = q.taskletengine.get(os.path.join(basedir, 'impl', 'authenticate'))
+    _authorize = q.taskletengine.get(os.path.join(basedir, 'impl', 'authorize'))
+    
+    def checkAuthentication(self, request, domain, service, methodname, args, kwargs):
+        if not request.username or not request.password: return False
+        tags = ('authenticate',)
+        params = dict()
+        params['request'] = request
+        params['domain'] = domain
+        params['service'] = service
+        params['methodname'] = methodname
+        params['args'] = args
+        params['kwargs'] = kwargs
+        _authenticate.execute(params, tags=tags)
+        return params.get('result', False)
+
+    def checkAuthorization(self, criteria, request, domain, service, methodname, args, kwargs):
+        tags = ('authorize',)
+        params = dict()
+        params['criteria'] = criteria
+        params['request'] = request
+        params['domain'] = domain
+        params['service'] = service
+        params['methodname'] = methodname
+        params['args'] = args
+        params['kwargs'] = kwargs
+        _authorize.execute(params, tags=tags)
+        return params.get('result', False)
