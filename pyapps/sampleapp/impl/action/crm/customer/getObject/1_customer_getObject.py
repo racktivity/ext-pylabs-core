0847a90e5bcdae7597f2487c6fb1ce4967a448a9
grypdonp <devnull@localhost>
2011-04-05 14:26:18 +0200

    Copied lead getObject implemenation to customer getObject

--- pyapps/sampleapp/impl/action/crm/customer/getObject/1_customer_getObject.py
+++ pyapps/sampleapp/impl/action/crm/customer/getObject/1_customer_getObject.py
@@ -1,12 +1,12 @@
-__author__ = 'incubaid'
-__priority__= 3
+__author__ = "Incubaid"
+
+TYPE = "customer"
+GUID_FIELD = "%sguid" % TYPE
 
 def main(q, i, p, params, tags):
-    from pymodel.serializers import ThriftSerializer
-    import base64
-    
-    customer = p.api.model.crm.customer.get(params['rootobjectguid'])
-    params['result'] = base64.encodestring(ThriftSerializer.serialize(customer))
+    if GUID_FIELD not in params:
+        raise ValueError("Cannot retrieve %s, there is no %s key in the params" % (TYPE, GUID_FIELD))
+    guid = params[GUID_FIELD]
 
-def match(q, i, p, params, tags):
-    return True
+    o = p.api.model.crm.customer.get(guid)
+    params['result'] = o
