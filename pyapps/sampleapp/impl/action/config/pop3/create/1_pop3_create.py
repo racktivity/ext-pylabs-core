7c1d1506b89ceb1196a8b30dfc66248faedc308b
Jens Geiregat <jens.geiregat@incubaid.com>
2011-04-06 12:01:18 +0200

    Use api.config.mail.pop3 instead of api.model.config.pop3

--- pyapps/sampleapp/impl/action/config/pop3/create/1_pop3_create.py
+++ pyapps/sampleapp/impl/action/config/pop3/create/1_pop3_create.py
@@ -1,11 +1,11 @@
 __author__ = 'incubaid'
 
 def main(q, i, p, params, tags):
-    pop3 = p.api.model.config.pop3.new()
+    pop3 = p.api.config.mail.pop3.new()
     pop3.name = params['server']
     pop3.login = params['login']
     pop3.password = params['password']
-    p.api.model.config.pop3.save(pop3)
+    p.api.config.mail.pop3.save(pop3)
     params['result'] = True
     
 def match(q, i, p, params, tags):
--- pyapps/sampleapp/impl/action/config/pop3/find/1_pop3_find.py
+++ pyapps/sampleapp/impl/action/config/pop3/find/1_pop3_find.py
@@ -1,14 +1,14 @@
 __author__ = 'incubaid'
 
 def main(q, i, p, params, tags):
-    filterObject = p.api.model.config.pop3.getFilterObject()
+    filterObject = p.api.config.mail.pop3.getFilterObject()
 
     fields = ('server','login')
     for key,value in params.iteritems():
         if key in fields and not value in (None, ''):
             filterObject.add('config_view_pop3_list', key, value)
           
-    result = p.api.model.config.pop3.find(filterObject)
+    result = p.api.config.mail.pop3.find(filterObject)
     params['result'] = result
 
     
