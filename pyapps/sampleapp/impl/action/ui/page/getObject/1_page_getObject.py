ae5b923834380a3539911af2ef7eb2a1171610f5
tdeconin <devnull@localhost>
2011-04-06 11:48:37 +0200

    morestuff

--- pyapps/sampleapp/impl/action/ui/page/getObject/1_page_getObject.py
+++ pyapps/sampleapp/impl/action/ui/page/getObject/1_page_getObject.py
@@ -1,8 +1,10 @@
+
+
 def main(q, i, p, params, tags):
     from pymodel.serializers import ThriftSerializer
     import base64
-    
-    page = p.api.model.ui.page.getObject(params['rootobjectguid'])
+   
+    page = p.api.model.ui.page.get(params['rootobjectguid'])
     params['result'] = base64.encodestring(page.serialize(ThriftSerializer))
     
 def match(q, i, p, params, tags):
