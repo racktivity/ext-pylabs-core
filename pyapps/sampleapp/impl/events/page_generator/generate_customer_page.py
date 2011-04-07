a2fce646ce99bc6415cefa3c5c4736bce091cdc3
grypdonp <devnull@localhost>
2011-04-07 15:10:17 +0200

    Modified page generation to include wizards + grid + added Home page for CRM space

--- pyapps/sampleapp/impl/events/page_generator/generate_customer_page.py
+++ pyapps/sampleapp/impl/events/page_generator/generate_customer_page.py
@@ -10,32 +10,56 @@ def main(q, i, p, params, tags):
 *   **VAT:** %(vat)s
 *   **Status:** %(status)s
 
-<div class="macro macro_actions"></div>
+<div class="macro macro_sqlgrid">
+    {
+        "dbconnection": "sampleapp",
+        "table": "crm_view_lead_list",
+        "schema": "crm",
+        "fields": ["name", "source", "type", "status"],
+        "sort": "name",
+        "pagesize": 10,
+        "width": 600,
+        "height": 490,
+        "fieldwidth": {
+            "name": 120,
+            "source": 120,
+            "type": 120,
+            "status": 120
+        }
+    }
 
-<form method="post" action="#/action/customer_edit">
-    <button type="submit" title="Edit customer"></button>
-</form>"""
+</div>
+
+<div class="macro macro_wizardactions"></div>
+
+<button onclick="start('sampleapp' ,'crm' ,'customer_edit','%(portalip)s', success)">Edit</button>
+"""
     customer = p.api.action.crm.customer.getObject(guid)
     searchresult = p.api.action.ui.page.find(name="customer_detail_%s"%guid)['result']
+    parentpage = p.api.action.ui.page.find(name="Home", space="crm")['result'][0]
+    portalip = q.system.net.getIpAddress(q.system.net.getNics(up=True)[1])[0][0]
+    
     if searchresult:
         p.api.action.ui.page.update(
             guid,
             "customer_detail_%s" % guid,
             "crm",
             "customer",
-            None,
+            parentpage,
             "crm customer",
             template % {"name": customer.name, "login": customer.login, "password": customer.password, "email": customer.email,
-                                          "address": customer.address, "vat": customer.vat, "status": customer.status})
+                                          "address": customer.address, "vat": customer.vat, "status": customer.status,
+                                          "portalip": portalip})
     else:
         p.api.action.ui.page.create(
             "customer_detail_%s" % guid,
             "crm",
             "customer",
-            None,
+            parentpage,
             "crm customer",
             template % {"name": customer.name, "login": customer.login, "password": customer.password, "email": customer.email,
-                                          "address": customer.address, "vat": customer.vat, "status": customer.status})
+                                          "address": customer.address, "vat": customer.vat, "status": customer.status,
+                                          "portalip": portalip})
         
 def match(q, i, params, tags):
     return params["eventKey"]=='pylabs.event.sampleapp.osis.store.crm.customer'
