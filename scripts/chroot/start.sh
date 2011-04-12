b1d36aaf9bc4bfe93fc210cfadeb1e2118864d52
Jens Geiregat <jens.geiregat@incubaid.com>
2011-04-12 09:55:57 +0200

    Remove unneeded and broken lines from the setup.sh chroot script

--- scripts/chroot/start.sh
+++ scripts/chroot/start.sh
@@ -4,7 +4,3 @@
 
 for i in /sys /dev /tmp /proc /dev/pts; do sudo mount -o bind "${i}" "${FULL_CHROOT_PATH}/${i}"; done
 sudo cp /etc/hosts "${FULL_CHROOT_PATH}/etc/hosts"
-
-sudo cp /srv/chroot/install.sh "${FULL_CHROOT_PATH}/install.sh"
-sudo chmod u+x "${FULL_CHROOT_PATH}/install.sh"
-
