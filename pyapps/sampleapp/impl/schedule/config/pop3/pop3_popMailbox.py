6beb5a6e83f82d3330c1fbbfb979da1a1c859ee5
Mohammad Nour <mojo@incubaid.com>
2011-04-27 16:26:45 +0200

    POP3 processor of sampleapp is not working
    - Finalized the POP3 scheduled task.
    -# Still the cron job does not send the correct last time execution, which I will fix in another bug.
    -# For now the POP3 scheduled task runs every minute.
    
    References: PYLABS-35.

--- pyapps/sampleapp/impl/schedule/config/pop3/pop3_popMailbox.py
+++ pyapps/sampleapp/impl/schedule/config/pop3/pop3_popMailbox.py
@@ -1,10 +1,13 @@
 __tags__ = 'schedule'
 __author__ = 'incubaid'
 
-def _extractMessage(mail_elements):
+def _extractMessage(q, mail_elements):
     """
     A helper method to retrieve the message text from an e-mail message
     """
+    # TODO - MNOUR: We should provide another way of parsing data out of a mail message which is not dependent on finding boundaries.
+    #               I know it but I need to finish the full cycle first and then I will get back to it.
+    import os
     if not mail_elements:
         q.errorconditionhandler.raiseError('Ivalid value %s.' %mail_elements)
 
@@ -16,12 +19,12 @@ def _extractMessage(mail_elements):
         q.errorconditionhanlder.raiseError('Found more than one boundary header.')
 
     boundary = boundry_headers[0].split('=')[1]
-    start = mail_elements.find('--%s' %boundary) + 3
-    end = mail_elements.find('--%s' %boundary, satrt + 1)
-    if satrt == -1 or end == -1:
+    start = mail_elements.index('--%s' %boundary) + 3
+    end = mail_elements.index('--%s' %boundary, start + 1)
+    if start == -1 or end == -1:
         q.errorconditionhandler.raiseError('Could not find boundary to extract and read e-mail message.')
 
-    return '\n'.join([mail_elements[index] for index in range(start, end) if mail_elements[index]])
+    return os.linesep.join([mail_elements[index] for index in range(start, end) if mail_elements[index]])
 
 def main(q, i, p, params, tags):
     import poplib
@@ -29,23 +32,31 @@ def main(q, i, p, params, tags):
     view = '%s_view_%s_list' %('mail', 'pop3')
     filter = p.api.config.mail.pop3.getFilterObject()
     filter.add(view, 'server', 'pop.gmail.com')
-    filter.add(view, 'login', 'analytics@incubaid.com')
+    filter.add(view, 'login', 'nourm@incubaid.com')
     pop3guids = p.api.config.mail.pop3.find(filter)
     if len(pop3guids) > 1:
         q.errorconditionhandler.raiseError("Found more than one POP3 mailbox configured for '%s' on '%s' server." %('nourm@incubaid.com', 'pop.gmail.com'))
 
+    if len(pop3guids) < 1:
+        q.errorconditionhandler.raiseError('Could not find configured POP3 mailbox.')
+
     pop3guid = pop3guids[0]
-    pop3 = p.api.config.mail.pop3.getObject(pop3guid)
+    pop3 = p.api.config.mail.pop3.get(pop3guid)
     mailbox = poplib.POP3_SSL(pop3.server)
     mailbox.user(pop3.login)
     mailbox.pass_(pop3.password)
     numMessages = len(mailbox.list()[1])
+    q.logger.log('[DEBUG] - Found %d message(s).' %numMessages, level=3)
     for i in range(numMessages):
-        for mail_elements in mailbox.retr(i+1)[1]:
-            mail = _extractMessage(mail_elements)
-            q.logger.log(mail, level=3)
-            p.events.publish('pylabs.event.sampleapp.email', 'mail:%s' %base64.encodestring(mail))
+        mail_elements = mailbox.retr(i+1)[1]
+        mail = _extractMessage(q, mail_elements)
+        q.logger.log('[DEBUG] - Extracted message nr#%d: %s' %((i+1), mail), level=3)
+        # TODO - MNOUR: Why we should tigh events to its source. The event processor should be able to receive events
+        #               from different sources. I would change that later.
+        p.events.publish('pylabs.event.sampleapp.email', 'mail:%s' %base64.encodestring(mail))
 
 def match(q, i, params, tags):
+    # TODO - MNOUR: For now the match always return True cause there is an error in the sent last execution time. It is always sent as 0.
     import time
-    return (params['taskletlastexecutiontime']  + 300 >= time.time())
+    # return (params['taskletlastexecutiontime']  + 300 >= time.time())
+    return True
