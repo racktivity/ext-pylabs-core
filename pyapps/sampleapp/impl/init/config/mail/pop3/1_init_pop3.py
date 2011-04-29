011d257b191b82908c1694535fa76382a6b10bcf
Jo De Boeck <jdeboeck@aserver.com>
2011-04-29 21:11:15 +0200

    Undo change

--- pyapps/sampleapp/impl/init/config/mail/pop3/1_init_pop3.py
+++ pyapps/sampleapp/impl/init/config/mail/pop3/1_init_pop3.py
@@ -2,5 +2,5 @@ def main (q, i, p, params, tags):
     connection = p.application.getAPI(params['appname']).action
     # TODO - MNOUR: The domain should not be config. This will be solved in another bug.
     q.logger.log("[INIT-POP3] - Creating a POP3 mail box '%s' on '%s' server" %('analytics@incubaid.com', 'pop.gmail.com'), level=3)
-    #connection.config.pop3.create('pop.gmail.com', 'analytics@incubaid.com', 'somepasswd')
+    connection.config.pop3.create('pop.gmail.com', 'analytics@incubaid.com', 'somepasswd')
     params['result'] = True
