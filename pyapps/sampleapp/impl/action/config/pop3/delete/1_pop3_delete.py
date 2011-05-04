b8364df12b1ee5418c257ea8eb0547a64d996ce3
boctor <devnull@localhost>
2011-05-04 19:27:11 +0300

    POP3 configuration model element does not have a delete action implementation: - created delete action
    PYLABS-51

---
+++ pyapps/sampleapp/impl/action/config/pop3/delete/1_pop3_delete.py
@@ -0,0 +1,9 @@
+__author__ = 'incubaid'
+
+def main(q, i, p, params, tags):
+    
+    p.api.config.mail.pop3.delete(params['rootobjectguid'])
+    param['result'] = True
+    
+def match(q, i, p, params, tags):
+    return True
