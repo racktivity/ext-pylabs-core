7f08863eb1f5775a81301a68a94337c484f65645
Mohammad Nour <mojo@incubaid.com>
2011-05-12 17:18:05 +0200

    - Correcting a bug in 1_pop3_delete.

--- pyapps/sampleapp/impl/action/config/pop3/delete/1_pop3_delete.py
+++ pyapps/sampleapp/impl/action/config/pop3/delete/1_pop3_delete.py
@@ -2,7 +2,7 @@ __author__ = 'incubaid'
 
 def main(q, i, p, params, tags):
     
-    p.api.config.mail.pop3.delete(params['rootobjectguid'])
+    p.api.config.mail.pop3.delete(params['pop3guid'])
     param['result'] = True
     
 def match(q, i, p, params, tags):
