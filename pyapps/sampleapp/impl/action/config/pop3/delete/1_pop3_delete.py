161d4cd081f4c587dc8d6cfb84d255ac0fd540fc
Mohammad Nour <mojo@incubaid.com>
2011-05-12 17:33:38 +0200

    - Correcting a typo.

--- pyapps/sampleapp/impl/action/config/pop3/delete/1_pop3_delete.py
+++ pyapps/sampleapp/impl/action/config/pop3/delete/1_pop3_delete.py
@@ -3,7 +3,7 @@ __author__ = 'incubaid'
 def main(q, i, p, params, tags):
     
     p.api.config.mail.pop3.delete(params['pop3guid'])
-    param['result'] = True
+    params['result'] = True
     
 def match(q, i, p, params, tags):
     return True
