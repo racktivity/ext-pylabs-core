0098d4f6901fb2db7830234467151d247b1bce0b
Mohammad Nour <mojo@incubaid.com>
2011-05-19 19:05:08 +0200

    API(s) generation is screwed specially when it comes to Configuration Model related API(s) generation
    - Fixing a test failure.
    
    References: PYLABS-45.

--- pyapps/sampleapp/impl/action/mail/pop3/create/1_pop3_create.py
+++ pyapps/sampleapp/impl/action/mail/pop3/create/1_pop3_create.py
@@ -5,7 +5,7 @@ def main(q, i, p, params, tags):
     pop3.server = params['server']
     pop3.login = params['login']
     pop3.password = params['password']
-    p.api.model.config.pop3.save(pop3)
+    p.api.config.mail.pop3.save(pop3)
     params['result'] = pop3.guid
     
 def match(q, i, p, params, tags):
