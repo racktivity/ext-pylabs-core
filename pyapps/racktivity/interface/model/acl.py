63a5415588f2d702de26f26e3f857e1d8a9a2010
rami <rami.helmy@aserver.com>
2011-07-17 16:19:57 +0200

    Fix the enumerations reference problem RTWEBAPP-1131

--- pyapps/racktivity/interface/model/acl.py
+++
@@ -1,6 +0,0 @@
-from pylabs.baseclasses.BaseEnumeration import BaseEnumeration
-import pymodel as model
-
-class acl(model.RootObjectModel):
-    # A dictionary in the form {'group1_action1':None, 'group2_action1':None, 'group1_action2': None}
-    cloudusergroupactions = model.Dict(model.String(), thrift_id=1)
---
+++ pyapps/racktivity/interface/model/racktivity/clouduserrole.py
@@ -0,0 +1,18 @@
+from enumerations import *
+import pymodel as model
+from acl import acl
+
+# @doc a group of cloudusers
+class clouduserrole(model.RootObjectModel):
+
+    #@doc name of clouduserrole
+    name = model.String(thrift_id=1)
+
+    #@doc optional description of clouduserrole
+    description = model.String(thrift_id=2)
+    
+    #@doc series of tags format
+    tags = model.String(thrift_id=3)
+
+    #@doc access control list
+    acl = model.Object(acl,thrift_id=4)
