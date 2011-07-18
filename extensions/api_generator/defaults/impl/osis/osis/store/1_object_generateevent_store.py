e85bb611a7ae3563403b396f64c548739405dd63
Majid Serag <majid.serag@gmail.com>
2011-07-18 18:42:23 +0300

    Changed 1_object_generateevent_store.py.

--- extensions/api_generator/defaults/impl/osis/osis/store/1_object_generateevent_store.py
+++ extensions/api_generator/defaults/impl/osis/osis/store/1_object_generateevent_store.py
@@ -2,5 +2,4 @@
 def main(q, i, p, params, tags):
     root = params['rootobject']
     domain = params['domain'] 
-    appname = '$appname'
-    p.events.publish('pylabs.event.%s.osis.store.%s.%s' % ( appname, domain, root.PYMODEL_MODEL_INFO.name), root.guid)
+    p.events.publish('pylabs.event.%s.osis.store.%s.%s' % ( p.api.appname, domain, root.PYMODEL_MODEL_INFO.name), root.guid)
