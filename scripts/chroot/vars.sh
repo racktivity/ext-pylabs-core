e910f19fc190a8d903beb7343c600ee33a196a02
mazmy <devnull@localhost>
2011-07-26 15:49:37 +0200

    revert bad commit

--- scripts/chroot/vars.sh
+++ scripts/chroot/vars.sh
@@ -1,5 +1,5 @@
-FULL_CHROOT_PATH="/opt/maverick"
-FULL_CLEAN_CHROOT_PATH="/opt/maverick.bak"
+FULL_CHROOT_PATH="/srv/chroot/sandbox"
+FULL_CLEAN_CHROOT_PATH="/srv/chroot/clean-sandbox"
 if [ -z "${UBUNTU_REPO}" ]; then
     UBUNTU_REPO="http://archive.ubuntu.com/ubuntu/"
 fi
