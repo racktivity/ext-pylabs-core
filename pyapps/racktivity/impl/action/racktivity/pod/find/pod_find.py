2f24dd615350b5c057dcdedef749ad81b52ab4fa
mina_magdy <devnull@localhost>
2011-07-27 07:23:40 -0700

    Fixes in pod/row find tasklets

--- pyapps/racktivity/impl/action/racktivity/pod/find/pod_find.py
+++ pyapps/racktivity/impl/action/racktivity/pod/find/pod_find.py
@@ -5,8 +5,7 @@ def main(q, i, p, params, tags):
     params['result'] = {'returncode':False}
     from rootobjectaction_lib import rootobjectaction_find
     params['result'] = {'returncode': True,
-                        'guidlist': rootobjectaction_find.pod_find(name=params['name'], alias=params['alias'], roomguid=params['roomguid'], \
-                                                                   rackguid=params['rackguid'], tags=params['tags'])}
+                        'guidlist': rootobjectaction_find.find("pod", name=params['name'], alias=params['alias'], roomguid=params['roomguid'], tags=params['tags'])}
 
 def match(q, i, params, tags):
     return True
