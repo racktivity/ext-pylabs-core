85adc6608a1e23e7e407f3d01b4a42077f7abb1c
rami <rami.helmy@aserver.com>
2011-07-28 17:21:00 +0200

    Fix the clouduser list action RTWEBAPP-1138

--- lib/rootobjectaction_lib/rootobjectaction_list.py
+++ lib/rootobjectaction_lib/rootobjectaction_list.py
@@ -75,7 +75,7 @@ def clouduser_list(clouduserguid=""):
     removekeys = ('viewguid','version')
     results = list()
     groupquery = """
-    select distinct groups.guid, groups.name from cloudusergroup.racktivity_view_cloudusergroup_clouduser as groups
+    select distinct groups.guid, groups.name from racktivity_cloudusergroup.racktivity_view_cloudusergroup_clouduser as groups
     where clouduserguid = '%s'
     """
     
