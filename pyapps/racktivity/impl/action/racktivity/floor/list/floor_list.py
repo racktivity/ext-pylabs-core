03671a6d2f7b9cf885c7a426d1196daf63d55af7
mazmy <devnull@localhost>
2011-07-31 16:17:31 +0200

    add more arguments to list method

--- pyapps/racktivity/impl/action/racktivity/floor/list/floor_list.py
+++ pyapps/racktivity/impl/action/racktivity/floor/list/floor_list.py
@@ -2,11 +2,11 @@ __author__ = 'racktivity'
 
 def main(q, i, p, params, tags):
     params['result'] = {'returncode':False}
-    floorguid = params['floorguid'] or None
     from rootobjectaction_lib import rootobjectaction_list
     
     params['result'] = {'returncode': True,
-                        'floorinfo': rootobjectaction_list.floor_list(floorguid=floorguid)}
+                        'floorinfo': rootobjectaction_list.floor_list(floorguid=params['floorguid'],
+                                                                      datacenterguid=params['datacenterguid'])}
 
 def match(q, i, params, tags):
     return True
