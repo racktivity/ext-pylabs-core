4f9e41d68088c1cb19f94aa021c62935d7c41450
Nicolas Trangez <ikke@nicolast.be>
2011-08-18 14:27:44 +0200

    Rework messaging.toolStripNonAsciFromText
    The toolStripNonAsciFromText (sic) function is called quite a lot inside the
    logging subsystem, and as such has a significant performance impact. This commit
    reworks the implementation on strings to use the more efficient str.translate
    method.
    
    In case non-str arguments are passed (e.g. a unicode instance), the old
    implementation is still used. unicode instances do have a 'translate' method,
    but creating a complete translation table doesn't sound like a good idea.
    
    Some performance numbers:
    
    >>> # Some helpers to create a random string
    >>> import random
    
    >>> validChars = [10] + range(32, 127)
    >>> # Don't do this in production code...
    >>> invalidChars = range(0, 10) + range(11, 32) + range(127, 255)
    
    >>> genChar = lambda c: lambda: chr(random.choice(c))
    >>> genValidChar = genChar(validChars)
    >>> genInvalidChar = genChar(invalidChars)
    
    >>> genString = lambda l: ''.join(
    ...     random.choice((genValidChar(), genInvalidChar(), )) for _ in xrange(l))
    
    >>> import messages
    >>> old = messages._old_toolStripNonAsciFromText
    >>> new = messages.toolStripNonAsciFromText
    
    >>> s = genString(1024)
    >>> old(s) == new(s)
    True
    
    >>> # Test a long string
    >>> %timeit old(s)
    1000 loops, best of 3: 269 us per loop
    >>> %timeit new(s)
    100000 loops, best of 3: 6.79 us per loop
    
    >>> # Test short strings
    >>> %timeit old('abc')
    100000 loops, best of 3: 2.23 us per loop
    >>> %timeit new('abc')
    1000000 loops, best of 3: 909 ns per loop
    
    >>> # Test Unicode
    >>> %timeit old(u'abc')
    100000 loops, best of 3: 2.42 us per loop
    >>> %timeit new(u'abc')
    100000 loops, best of 3: 3.16 us per loop
    >>> # New version is slightly slower (because it calls the old one, with an
    >>> # extra type check), but unicode isn't handled correctly by the old
    >>> # implementation anyway (and I wouldn't know how to fix it)

--- core/messages/__init__.py
+++ core/messages/__init__.py
@@ -1,4 +1,17 @@
 import string
 
-def toolStripNonAsciFromText(text):
+# For backward compatibility with unicode strings, we keep the old version
+# around and use it accordingly
+def _old_toolStripNonAsciFromText(text):
     return string.join([char for char in str(text) if ((ord(char)>31 and ord(char)<127) or ord(char)==10)],"")
+
+
+_deletions = range(0, 10) + range(11, 32) + range(127, 255)
+_deletionChars = ''.join(chr(c) for c in _deletions)
+del _deletions
+
+def toolStripNonAsciFromText(text, _dontUse=_deletionChars):
+    if isinstance(text, str):
+        return text.translate(None, _dontUse)
+    else:
+        return _old_toolStripNonAsciFromText(text)
