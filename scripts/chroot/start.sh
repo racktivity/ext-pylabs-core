386a4d258a75ca40b646683f85c5354a7c0e2c49
Jo De Boeck <jdeboeck@aserver.com>
2011-11-18 13:25:59 +0200

    Start rabbitmq if availble when starting chroot

--- scripts/chroot/start.sh
+++ scripts/chroot/start.sh
@@ -6,3 +6,6 @@ set -e
 
 for i in /sys /dev /tmp /proc ; do sudo mount -o rbind "${i}" "${FULL_CHROOT_PATH}/${i}"; done
 sudo cp /etc/hosts "${FULL_CHROOT_PATH}/etc/hosts"
+if [ -e "${FULL_CHROOT_PATH}/etc/init.d/rabbitmq-server" ]; then
+    sudo chroot "${FULL_CHROOT_PATH}" /etc/init.d/rabbitmq-server start
+fi
