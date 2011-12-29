b455b0fd10d1f77be6f13c820eb7a2e5f74b39e2
Jo De Boeck <jdeboeck@aserver.com>
2011-12-29 10:53:37 +0200

    Change events to use p.api.events

--- extensions/api_generator/defaults/impl/osis/osis/delete/3_object_generateevent_delete.py
+++ extensions/api_generator/defaults/impl/osis/osis/delete/3_object_generateevent_delete.py
@@ -3,4 +3,4 @@ def main(q, i, p, params, tags):
     guid = params['rootobjectguid']
     type = params['rootobjecttype']
     domain = params['domain'] 
-    p.events.publish('pylabs.event.%s.osis.delete.%s.%s' % (p.api.appname, domain, type), guid)
+    p.api.events.publish('pylabs.event.%s.osis.delete.%s.%s' % (p.api.appname, domain, type), guid)
--- extensions/api_generator/defaults/impl/osis/osis/store/1_object_generateevent_store.py
+++ extensions/api_generator/defaults/impl/osis/osis/store/1_object_generateevent_store.py
@@ -3,4 +3,4 @@ def main(q, i, p, params, tags):
     root = params['rootobject']
     type = params['rootobjecttype']
     domain = params['domain'] 
-    p.events.publish('pylabs.event.%s.osis.store.%s.%s' % (p.api.appname, domain, type), root.guid)
+    p.api.events.publish('pylabs.event.%s.osis.store.%s.%s' % (p.api.appname, domain, type), root.guid)
