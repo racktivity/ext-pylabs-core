3b1a857aa2be2d4e32e6a2f7ca2fd240d68954ca
mazmy <devnull@localhost>
2012-01-22 15:31:10 +0200

    update creationdate

---
+++ extensions/api_generator/defaults/impl/osis/osis/store/4_object_store.py
@@ -0,0 +1,8 @@
+__author__ = 'incubaid'
+from pymodel.serializers import ThriftSerializer
+import time
+
+def main(q, i, p, params, tags):
+    root = params['rootobject']
+    if not root.creationdate:
+        root.creationdate = str(int(time.time()))
