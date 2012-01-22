2bb993a7bcf239543ffe4b762a66973deb5862c3
mazmy <devnull@localhost>
2012-01-22 15:57:37 +0200

    skip core objects

--- extensions/api_generator/defaults/impl/osis/osis/store/4_object_store.py
+++ extensions/api_generator/defaults/impl/osis/osis/store/4_object_store.py
@@ -6,3 +6,6 @@ def main(q, i, p, params, tags):
     root = params['rootobject']
     if not root.creationdate:
         root.creationdate = str(int(time.time()))
+        
+def match(q, i, p, params, tags):
+    return params['domain'] != 'core'
